b944699c8e7d9080f20b67cc4079f5589e2d6a4c
MUHAMMADaKbAr925 <Muhammadakbartr11@gmail.com>
2024-08-23 19:12:45 +0300

    second commit, added some features

--- react-todo-app/src/components/Todos/index.js
+++ react-todo-app/src/components/Todos/index.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 
+
+const intialState = [
+  {id: 1, message : "wake up"},
+  {id: 2, message : "walk the dog"},
+  {id: 3, message : "go to bed"},
+
+
+
+]
+
 const Todos = () => {
+    const [todoList, setTodoList] = useState(intialState)
+
+
   return (
     <div>
     
-    <TodoForm />
-    <TodoList />
+    <TodoForm todos={todoList}  setTodos={setTodoList} />
+    <TodoList todos = {todoList} />
 
     </div>
   )
